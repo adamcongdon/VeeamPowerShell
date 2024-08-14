e6a3fa2fc5026b7f4c98f63a7fac9e12246f3ba8
Adam.Congdon <cac8907@gmail.com>
2024-08-14 15:18:13 -0400

    added memory management (slows down the program), added logging to help indicate that the program is still working.

--- VBR/NAS/VBR-NAS-ExportBackedUpFilesAndSearch.ps1
+++ VBR/NAS/VBR-NAS-ExportBackedUpFilesAndSearch.ps1
@@ -40,39 +40,140 @@ function Select-ItemsPerCSV {
     }
     return $fCount
 }
+# function to log message to console with timestamp
+function Log-Message($message) {
+    Write-Host "$(Get-Date) - $message"
+}
+
 # function to get content of folders
 function Get-FLRContent($folder) {
     $files = New-Object System.Collections.Generic.List[Object]
     foreach ($item in $folder) {
         $res = Get-VBRUnstructuredBackupFLRItem -Session $flr -folder $item
         # foreach item in $res, if type is file add to $files
-        foreach ($r in $res) {
-            if ($r.Type -eq "File") {
-                $files.add($r)# += $r
-            }
-            elseif ($r.Type -eq "Folder") {
-                $f = Get-FLRContent $r
+
+        $fi = $res | Where-Object { $_.Type -eq "File" }
+        if($fi.Count -eq 1) {
+            # echo found file count
+            Log-Message("New files found: " + $fi.Count)
+            $files.Add($fi)
+        }
+        elseif($fi.Count -gt 1) {
+            # echo files count
+            Log-Message("New files found: " + $fi.Count)
+        $files.AddRange($fi)
+        }
+        $global:TotalFileCount += $fi.Count
+
+        #if global total equal to or greater than 1 million, export to csv and flush $files variable
+        if ($global:TotalFileCount -ge 1000000) {
+            $files = Export-FLRContent $files $destination $fCount
+            $files = New-Object System.Collections.Generic.List[Object]
+        }
+
+        $fo = $res | Where-Object { $_.Type -eq "Folder" }
+        if ($fo.Count -gt 0) {
+            # echo folders count
+           Log-Message("Folders To Sort: "+ $fo.Count)
+            $f = Get-FLRContent $fo
                 if ($f.count -gt 1) {
+                    #echo files count
+                    Log-Message("New files found: "+ $f.Count)
                     $files.AddRange($f)
                 }
                 elseif ($f.count -eq 1) {
+                    #echo files count
+                    Log-Message("New files found: " + $f.Count)
                     $files.add($f)
 
                 }
-                #$files.AddRange((Get-FLRContent $r))
-            }        
         }
+
+
+
+        # foreach ($r in $res) {
+        #     if ($r.Type -eq "File") {
+        #         $files.add($r)
+        #         # add total to global totals variable
+        #         $global:TotalFileCount++
+
+        #         #if global total equal to or greater than 1 million, export to csv and flush $files variable
+        #         if ($global:TotalFileCount -ge 1000000) {
+        #             $files = Export-FLRContent $files $destination $fCount
+        #             $files = New-Object System.Collections.Generic.List[Object]
+        #         }
+        #     }
+        #     elseif ($r.Type -eq "Folder") {
+        #         $f = Get-FLRContent $r
+        #         if ($f.count -gt 1) {
+        #             $files.AddRange($f)
+        #         }
+        #         elseif ($f.count -eq 1) {
+        #             $files.add($f)
+
+        #         }
+        #         #$files.AddRange((Get-FLRContent $r))
+        #     }        
+        # }
     }
-    # if($files.count -eq 0){
-    #     return $null    
-    # }
-    # if($files.Count -eq 1){
-    #     return @($files)
-    # }
-    # else{
+    #$files = Export-FLRContent $files $destination $fCount
+    Log-Message("Files Counted: " + $global:TotalFileCount)# -ForegroundColor Green
     return $files
 
     # }
+
+}
+# function Export-FLRContent($files, $destination, $fCount) {
+#     #check for files in $desintation and count the lines in the newest file
+#     $outputFiles = Get-ChildItem $destination | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+    
+#     $n = $job.Name
+#     #if files exist, get the line count, if no files exist, create a new file
+#     if ($outputFiles.Count -gt 0) {
+#         $lines = (Get-Content $outputFiles.FullName).Count
+#         if ($lines -gt $fCount) {
+#             $fileName = Create-NewOutpuFile $destination $n
+#         }
+#         else {
+#             $fileName = $outputFiles.FullName
+#         }
+#     }
+#     else {
+#         $lines = 0
+#         $fileName = Create-NewOutpuFile $destination $n
+#     }
+#     #if the line count is greater than $fCount, create a new file
+
+#     $files | Export-Csv -Path $fileName -NoTypeInformation -Append
+
+#     # zero out the $files variable
+
+#     $files = @()
+#     return $files
+# }
+# write a function similar to Export-FLRContent but splits the files into chunks of $fCount
+function Export-FLRContent($files, $destination, $fCount) {
+    $n = $job.Name
+    $date = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
+    $fileCounter = 0
+    if ($files.Count -gt $fCount) {
+        for ($i = 0; $i -lt $files.Count; $i += $fCount) {
+            $fileName = "$destination\files_$($n)_$($date)_$($fileCounter).csv"
+            $files[$i..($i + $fCount)] | Export-Csv -Path $fileName -NoTypeInformation
+            $fileCounter++
+        }
+    }
+    else {
+        $fileName = "$destination\files_$($n)_$($date).csv"
+        $files | Export-Csv -Path $fileName -NoTypeInformation
+    }
+    $files = @()
+    return $files
+}
+function Create-NewOutpuFile($destination, $jobName) {
+    $date = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
+    $fileName = "$destination\files_$($jobName)_$($date).csv"
+    return $fileName
 
 }
 function Set-DestinationDirectory {
@@ -84,7 +185,7 @@ function Set-DestinationDirectory {
     if ([string]::IsNullOrWhiteSpace($destination)) {
         $destination = $defaultDestination
     }
-
+    $destination = $destination + "\\" + $job.Name + "_" + (Get-Date -Format "yyyy-MM-dd-HH-mm-ss")
     if (-not (Test-Path $destination)) {
         New-Item -ItemType Directory -Path $destination | Out-Null
     }
@@ -95,7 +196,7 @@ function Set-DestinationDirectory {
 ##############################################################################################
 #print start time
 Write-Host "Script started at: $(Get-Date)" -ForegroundColor Green
-
+$fileCounter = 0
 # 1. Get all unstructured jobs into $jobs variable
 $jobs = Get-VBRJob | Where-Object { $_.JobType -eq "NasBackup" }
 
@@ -216,13 +317,21 @@ $timer = [System.Diagnostics.Stopwatch]::StartNew()
 # get base source from the backup:
 $baseName = Get-VBRUnstructuredBackupFLRItem -Session $flr
 
+$global:TotalFileCount = 0
 $filesResult = Get-FLRContent $baseName
 
+# if $filesResult count is not 0, export the remaining files to a csv file
+if ($filesResult.Count -gt 0) {
+    $filesResult = Export-FLRContent $filesResult $destination $fCount
+}
 #stop timer and print time taken
 $timer.Stop()
 Write-Host "Time taken to get files: $($timer.Elapsed.TotalMinutes) minutes" 
 # echo file count
-Write-Host "Number of files found: $($filesResult.Count)" -ForegroundColor Green
+#get file count from destination by counting lines in all files
+#$filesCount = Get-ChildItem $destination -Recurse | Measure-Object -Line | Select-Object -ExpandProperty Lines
+
+Write-Host "Number of files found: $($global:TotalFileCount)" -ForegroundColor Green
 
 
 
@@ -231,58 +340,60 @@ if ($filesResult.Count -eq 0) {
     Close-FLRSession $flr
     Exit
 }
-
-
-
-
-
-
-# Export files to destination directory
-Write-Host "Exporting files to $destination..."
-$n = $job.Name
-$files = $filesResult 
-$date = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
-if ($files.Count -gt $fCount) {
-    for ($i = 0; $i -lt $files.Count; $i += $fCount) {
-        
-        $fileName = "$destination\files_$($n)_$($date)_$($i).csv"
-        $files[$i..($i + $fCount)] | Export-Csv -Path $fileName -NoTypeInformation
-    }
-}
 else {
-    $fileName = "$destination\files_$($n)_$($date).csv"
-    $files | Export-Csv -Path $fileName -NoTypeInformation
+    Close-FLRSession $flr
 }
 
-Write-Host "Files exported to $fileName" -ForegroundColor Green
 
 
-#print end time
-Write-Host "Script ended at: $(Get-Date)" -ForegroundColor Green
-# Search for files
 
-while ($true) {
-    # Prompt for search files or exit and close FLR session
-    $search = Read-Host "Do you want to search for a file? (Y/N)"
-    if ($search -eq "N") {
-        Write-Host "Closing FLR session..." -ForegroundColor Yellow
-        Stop-VBRUnstructuredBackupFLRSession -Session $flr
 
-        break
-    }
-    if ($search -eq "Y") {
-        $searchString = Read-Host "Enter search string"
-        $searchResults = $files | Where-Object { $_.Name -like "*$searchString*" }
+# # Export files to destination directory
+# Write-Host "Exporting files to $destination..."
+# $n = $job.Name
+# $files = $filesResult 
+# $date = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
+# if ($files.Count -gt $fCount) {
+#     for ($i = 0; $i -lt $files.Count; $i += $fCount) {
         
-        $date2 = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
+#         $fileName = "$destination\files_$($n)_$($date)_$($i).csv"
+#         $files[$i..($i + $fCount)] | Export-Csv -Path $fileName -NoTypeInformation
+#     }
+# }
+# else {
+#     $fileName = "$destination\files_$($n)_$($date).csv"
+#     $files | Export-Csv -Path $fileName -NoTypeInformation
+# }
 
-        Write-Host "Number of search results found: $($searchResults.Count)" -foregroundcolor green
-        $searchResults | Export-Csv -Path "$destination\searchResults_$($n)_$($date2).csv" -NoTypeInformation
-        Write-Host "Search results saved to $destination\searchResults_$($n).$($date2).csv"
-    }
-    else {
-        Write-Host "Invalid input. Please enter Y or N"
-    }
+# Write-Host "Files exported to $fileName" -ForegroundColor Green
 
-}
+
+# #print end time
+# Write-Host "Script ended at: $(Get-Date)" -ForegroundColor Green
+# # Search for files
+
+# while ($true) {
+#     # Prompt for search files or exit and close FLR session
+#     $search = Read-Host "Do you want to search for a file? (Y/N)"
+#     if ($search -eq "N") {
+#         Write-Host "Closing FLR session..." -ForegroundColor Yellow
+#         Stop-VBRUnstructuredBackupFLRSession -Session $flr
+
+#         break
+#     }
+#     if ($search -eq "Y") {
+#         $searchString = Read-Host "Enter search string"
+#         $searchResults = $files | Where-Object { $_.Name -like "*$searchString*" }
+        
+#         $date2 = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
+
+#         Write-Host "Number of search results found: $($searchResults.Count)" -foregroundcolor green
+#         $searchResults | Export-Csv -Path "$destination\searchResults_$($n)_$($date2).csv" -NoTypeInformation
+#         Write-Host "Search results saved to $destination\searchResults_$($n).$($date2).csv"
+#     }
+#     else {
+#         Write-Host "Invalid input. Please enter Y or N"
+#     }
+
+# }
 
